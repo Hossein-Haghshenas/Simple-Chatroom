51fe0fc7df4b20f19e610a23f5b4cc9318e5aa45
Hossein <haghshenas3c@gmail.com>
2022-03-10 11:09:30 +0330

    Add animation for messages

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -8,7 +8,12 @@ const displayChat = (data, id) => {
 
   /* Create li for new Message  */
   const newMessages = document.createElement("li");
-  newMessages.classList.add("message");
+  /* set class and animation class */
+  newMessages.classList.add(
+    "message",
+    "animate__animated",
+    "animate__fadeInLeft"
+  );
   newMessages.setAttribute("data-id", id);
 
   /* Message name */
@@ -38,14 +43,25 @@ const displayChat = (data, id) => {
     return `${hour}:${minutes}`;
   }
 
-  /* delete message function */
+  /* delete message from database function */
   deleteMessage(newMessages);
 };
+
+/* delete chats from ui  */
 
 const deleteChat = (chat, id) => {
   const chats = document.querySelectorAll(".message");
   chats.forEach((elem) => {
-    elem.getAttribute("data-id") === id ? elem.remove() : elem;
+    if (elem.getAttribute("data-id") === id) {
+      /* set nimation class */
+      elem.classList.replace("animate__fadeInLeft", "animate__fadeOutRightBig");
+      /* remove */
+      setTimeout(() => {
+        elem.remove();
+      }, 100);
+    } else {
+      elem;
+    }
   });
 };
 
